91024a2553a13bfbe869f31ab45b909b90b57821
larsbenner <lars.benner@spartans.ut.edu>
2019-08-27 17:56:34 -0400

    day-2 homework first part

--- day2-homework/day2-hw.py
+++ day2-homework/day2-hw.py
@@ -3,17 +3,19 @@
 import sys
 import numpy
 
-"""Print all unique gene names from a t_data.ctab file"""
+"""parses txt file to get FLYbase ID and uniprot ID"""
 
 f = sys.argv[1]
 
 fly_base = []
 ac_uniprot = []
 for line in open(f):
+    """iterates through file"""
     if "DROME" and "FBgn" in line:
         fields = line.split()
         fly_base = fields[-1]
         ac_uniprot = fields[-2]
+        """prints parsed file"""
         print (fly_base, "\t", ac_uniprot)
     else:
         continue
